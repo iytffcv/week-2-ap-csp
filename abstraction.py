8345b767cfac7ffd52242b28122f598905a3ad60
tyuhgvbj <105885738+iytffcv@users.noreply.github.com>
2025-11-04 18:51:41 +0000

    abstraction

---
+++ abstraction.py
@@ -0,0 +1,22 @@
+
+#abstraction is when I hide 
+# the complex details for something
+# alot more simple
+
+#personal information
+# a function allows us to warp data or
+#define a funtction
+def personInformatriuon() :
+    question1 = input("how old are you?")
+    question2 = input("where do you live?")
+    question3 = input("what school do you go to?")
+    print(question1 + question2 + question3)
+personInformatriuon() 
+personInformatriuon() 
+personInformatriuon() 
+
+q1 = input("what year is it now?")
+q2 = input("what year were you born")
+answer = q1 - q2
+result = print(f'you are {answer}years old')
+
--- week_11_day2.py
+++ week_11_day2.py
@@ -16,20 +16,26 @@ favorite_number = 7
 
 #  Step 2: Practice String Operations
 # 1. Print your name in uppercase
-
+print(first_name.upper())
 
 # 2. Print how many letters are in your name
-
+print(len(first_name))
 
 # 3. Combine your name and favorite color into one message
-
+print(f"My (first_name) and my favorite color is {favorite_color}")
 
 #  Step 3: Math Practice
 # Use arithmetic operators with your favorite number
+print(f"My favorite number times 2 is {favorite_number * 2}.")
+print(f"My favorite number plus 10 is {favorite_number + 10}.")
+print(f"My favorite number squared is {favorite_number ** 2}.")
 
 
 #  Step 4: User Input Practice
 # Ask the user two questions and combine answers
+ser_food = input("What is your favorite food? ")
+user_place = input("Where do you want to travel? ")
+print(f"You love {user_food} and want to visit {user_place}!")
 
 
 
@@ -37,3 +43,6 @@ favorite_number = 7
 # ⚙️ Step 5: Final Challenge (combine it all)
 # Use math and strings together
 
+print(f"{first_name.upper()} loves {favorite_color}, enjoys eating {ser_food}, ")
+print f"and dreams of visiting {user_place}. "
+print( f"If you double their favorite number ({favorite_number}), you get {favorite_number * 2}!")
